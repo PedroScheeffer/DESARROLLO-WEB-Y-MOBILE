ad304c5de99775940baadd2c2856ea3a02744eb7
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-04-03 20:25:43 -0300

    add card working

--- UT1/TA5/scripts/main.js
+++ UT1/TA5/scripts/main.js
@@ -1,6 +1,8 @@
 const addTodoButtons = document.getElementsByClassName("add-todo-item");
 let tables = document.getElementsByClassName("table");
+const cardTemplate = document.getElementById("card-container");
 
+console.log(cardTemplate);
 for (let index = 0; index < addTodoButtons.length; index++) {
     const button = addTodoButtons[index];
     button.addEventListener('click', () => addCard(button));
@@ -11,16 +13,21 @@ function addCard(button) {
     console.log("add Card")
     let box = button.parentNode.parentNode;
     // crate the task
-    let card = document.createElement("div");
-    card.className = "card";
-    card.addEventListener('click', editCardPopUp());
     let cardlist = box.getElementsByClassName("card-list")[0];
-    cardlist.appendChild(card);
+    const cardClone = cardTemplate.cloneNode(true);
+    cardClone.removeAttribute("id");
+    console.log(cardClone);
+    cardlist.appendChild(cardClone);
 }
 
 function editCardPopUp(){
     console.log("edit card pop up")
-    
+    let text = prompt('Enter new text', 'New task');
+    if(text == null || text == ""){
+        return;
+    }
+    let card = this;
+    card.innerHTML = text;
 }
 
 // transform the input to a p and then create a new li with it input

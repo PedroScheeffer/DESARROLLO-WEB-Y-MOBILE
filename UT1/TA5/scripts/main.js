7bedcda742c0d182cfc56bddd8d3a1cfa3a21904
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-04-03 19:27:22 -0300

    Change of aproch

--- UT1/TA5/scripts/main.js
+++ UT1/TA5/scripts/main.js
@@ -1,43 +1,62 @@
 const addTodoButtons = document.getElementsByClassName("add-todo-item");
+let tables = document.getElementsByClassName("table");
+
 for (let index = 0; index < addTodoButtons.length; index++) {
     const button = addTodoButtons[index];
-    button.addEventListener('click', () => addTodo(button));
+    button.addEventListener('click', () => addCard(button));
+}
+
+
+function addCard(button) {
+    console.log("add Card")
+    let box = button.parentNode.parentNode;
+    // crate the task
+    let card = document.createElement("div");
+    card.className = "card";
+    card.addEventListener('click', editCardPopUp());
+    let cardlist = box.getElementsByClassName("card-list")[0];
+    cardlist.appendChild(card);
+}
+
+function editCardPopUp(){
+    console.log("edit card pop up")
+    
 }
 
 // transform the input to a p and then create a new li with it input
-function addTodo(button) {
-    // console.log("ejecuto")   
-    const card = button.parentNode.parentNode;
-    cardChilds = card.childNodes;
-    // TODO: arreglar esto 
-    for (const child of cardChilds) {
-        if (child.className === "todo-list") {
-            const todoList = child;
-            console.log("ol", todoList);
+// function addTodo(button) {
+//     // console.log("ejecuto")   
+//     const card = button.parentNode.parentNode;
+//     cardChilds = card.childNodes;
+//     // TODO: arreglar esto 
+//     for (const child of cardChilds) {
+//         if (child.className === "todo-list") {
+//             const todoList = child;
+//             console.log("ol", todoList);
 
-            const todoListItems = todoList.childNodes;
-            console.log("todoListItems", todoListItems);
-            let todoListItem = todoListItems[todoListItems.length - 2];
-            /*
-            for (const li of todoListItems) {
-                if (li.id === "todo-input") {
-                    todoListItem = li;
-                    break;
-                }
-            }
-            */
-            console.log("li", todoListItem);
+//             const todoListItems = todoList.childNodes;
+//             console.log("todoListItems", todoListItems);
+//             let todoListItem = todoListItems[todoListItems.length - 2];
+//             /*
+//             for (const li of todoListItems) {
+//                 if (li.id === "todo-input") {
+//                     todoListItem = li;
+//                     break;
+//                 }
+//             }
+//             */
+//             console.log("li", todoListItem);
             
-            const listItemInput = todoListItem.innerHTML;
-            console.log("input", listItemInput);
+//             const listItemInput = todoListItem.innerHTML;
+//             console.log("input", listItemInput);
 
-            let newPToReplaceLi = document.createElement("p");
-            newPToReplaceLi.value = listItemInput.value;
+//             let newPToReplaceLi = document.createElement("p");
+//             newPToReplaceLi.value = listItemInput.value;
 
-            todoList.appendChild(todoListItem);
-            todoListItem.replaceChild(newPToReplaceLi, listItemInput)
-        }
-    }
-}
+//             todoList.appendChild(todoListItem);
+//             todoListItem.replaceChild(newPToReplaceLi, listItemInput)
+//         }
+//     }
+// }
 
 // agregar funcion para agregar un nuevo item y otra para editar

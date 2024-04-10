515fc86fce809dda51d516f41b1378aa2a2d8035
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-04-10 18:21:46 -0300

    new html element loading system

--- UT1/TA5/scripts/main.js
+++ UT1/TA5/scripts/main.js
@@ -1,105 +1,153 @@
-let tables = document.getElementsByClassName("table");
-const cardTemplate = document.getElementById("card-container");
-const editMenuTemplate = document.getElementById("edit-card-menu-template");
+const cardTemplate = document.getElementById("card-template").cloneNode(true);
+document.getElementById("card-template").remove();
+const tableTempalte = document.getElementById("table-0").cloneNode(true);
+const editMenuDialog = document.getElementById("dialog-edit-menu");
 let selectedCard = null;
+tablesArray = [];
 
-addEvents();
-function addEvents(){
+InitializeApp();
+
+function InitializeApp() {
+
+    emptyHtmlTableContent();
+    newTable();
+    createHtmlElements();
+    addEvents();
+}
+
+function createHtmlElements() {
+    // creates all the tables from tablesArray
+    emptyHtmlTableContent();
+    for (let index = 0; index < tablesArray.length; index++) {
+        const table = tablesArray[index];
+        createHtmltable(table)
+    }
+}
+
+function emptyHtmlTableContent() {
+    tableContainer = document.getElementById("table-container");
+    while (tableContainer.firstChild) {
+        tableContainer.removeChild(tableContainer.firstChild);
+    }
+}
+function newTable() {
+    let table = {
+        index: tablesArray.length,
+        cards: [],
+    }
+    let card = {
+        title: "Title",
+        description: "Description",
+        assignedTo: "Assigned To",
+        startDate: "Start Date",
+        endDate: "End Date",
+        status: "Status",
+        priority: "Priority",
+        comments: ["Comentarios"],
+    }
+    table.cards[0] = card
+    tablesArray.push(table)
+    createHtmlElements()
+}
+
+function addCardToTable(table) {
+    let newCard = {
+        title: "Title",
+        description: "Description",
+        assignedTo: "Assigned To",
+        startDate: "Start Date",
+        endDate: "End Date",
+        status: "Status",
+        priority: "Priority",
+        comments: ["Comentarios"],
+    }
+    table.cards.push(newCard);
+    createHtmlElements();
+}
+
+function createHtmltable(table) {
+    const tableContainer = document.getElementById("table-container");
+    const newTable = tableTempalte.cloneNode(true);
+    newTable.display = "flex";
+    newTable.id = `table-${table.index}`
+    createHtmlCards(table.cards, newTable);
+    button = newTable.getElementsByClassName("add-todo-item")[0];
+    button.addEventListener('click', () => addCard(table));
+    tableContainer.appendChild(newTable);
+}
+function addCard(table) {
+    console.log("add Card")
+    addCardToTable(table);
+    createHtmlElements();
+}
+
+function createHtmlCards(cards, tableHtml) {
+    for (let index = 0; index < cards.length; index++) {
+        const card = cards[index];
+        const cardHtml = cardTemplate.cloneNode(true);
+        cardHtml.display = "flex"
+        updateCardHtml(cardHtml, card);
+        console.log(tableHtml);
+        let cardList = tableHtml.getElementsByClassName("card-list")[0];
+        cardList.appendChild(cardHtml);
+    }
+}
+
+function updateCardHtml(cardHtml, cardData) {
+    cardHtml.getElementsByClassName("card-title")[0].innerHTML = cardData.title;
+    cardHtml.getElementsByClassName("card-text")[0].innerHTML = cardData.description;
+    cardHtml.addEventListener('click', () => DialogEditCard(cardData));
+}
+
+function DialogEditCard(cardData) {
+    console.log("edit card pop up");
+    selectedCard = cardData;
+    dialogTitle = editMenuDialog.getElementsByClassName("dialog-title")[0];
+    dialogTitle.value = cardData.title;
+    menuTextArea = editMenuDialog.getElementsByTagName("textarea")[0];
+    menuTextArea.value = cardData.description;
+    editMenuDialog.showModal();
+}
+
+function closeAndSaveData() {
+    console.log("close")
+    dialogTitle = editMenuDialog.getElementsByClassName("dialog-title")[0];
+    selectedCard.title = dialogTitle.value;
+    dialogTitle.value = "";
+    menuTextArea = editMenuDialog.getElementsByTagName("textarea")[0];
+    selectedCard.description = menuTextArea.value;
+    menuTextArea.value = "";
+    selectedCard = null;
+
+    editMenuDialog.getElementsByTagName("textarea")[0].value = "";
+    editMenuDialog.close();
+    createHtmlElements();
+}
+
+function addEvents() {
     const addTodoButtons = document.getElementsByClassName("add-todo-item");
     for (let index = 0; index < addTodoButtons.length; index++) {
         const button = addTodoButtons[index];
         button.addEventListener('click', () => addCard(button));
     }
-    const addEditButtons = document.getElementsByClassName("edit-card");
-    for (let index = 0; index < addEditButtons.length; index++) {
-        const button = addEditButtons[index];
-        button.addEventListener('click', () => editCardPopUp(button.parentElement));
-    }
-    const editMenuCloseButton = editMenuTemplate.getElementsByClassName("box-menu-Button")[0];
+    // dialog pop up button 
+    const editMenuCloseButton = editMenuDialog.getElementsByClassName("box-menu-Button")[0];
     console.log(editMenuCloseButton);
-    editMenuCloseButton.addEventListener('click', () => close());
+    editMenuCloseButton.addEventListener('click', () => closeAndSaveData());
+    // add table button 
+    const addTableButton = document.getElementById("add-new-table");
+    addTableButton.addEventListener('click', () => newTable());
+    // Dark mode button 
+    const darkModeButton = document.getElementById("dark-mode");
+    darkModeButton.addEventListener('click', () => toggleDarkMode());
 }
 
-
-function addCard(button) {
-    console.log("add Card")
-    let box = button.parentNode.parentNode;
-    // crate the box
-    let cardlist = box.getElementsByClassName("card-list")[0];
-    const cardClone = cardTemplate.cloneNode(true);
-    cardClone.removeAttribute("id");
-    console.log(cardClone);
-    cardClone.addEventListener('click', () => editCardPopUp(cardClone));
-    cardlist.appendChild(cardClone);
-}
-
-function editCardPopUp(card){
-    console.log("edit card pop up");
-    selectedCard = card;
-    menuTextArea = editMenuTemplate.getElementsByTagName("textarea")[0];
-    menuTextArea.value = card.getElementsByClassName("card-text")[0].innerHTML;
-    editMenuTemplate.style.display = "flex";
-    /*
-    console.log("edit card pop up")
-    let text = prompt('Enter new text', 'New task');
-    while(text.length > 320){
-        text = prompt("The text was to long, max sizes is 320 characters", "New task");
+function toggleDarkMode() {
+    bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor == "var(--dark)") {
+        bodyStyle.backgroundColor = "var(--light)";
+    } else {
+        bodyStyle.backgroundColor = "var(--dark)";
     }
-    if(text == null || text == ""){
-        return;
-    }
-    let p = card.getElementsByClassName("card-text")[0];
-    p.innerHTML = text;
-
-    console.log(card);
-    */
-}
-
-function close(){
-    console.log("close")
-    menuTextArea = editMenuTemplate.getElementsByTagName("textarea")[0];
-    cardPElement = selectedCard.getElementsByClassName("card-text")[0];
-    cardPElement.innerHTML = menuTextArea.value; 
-    selectedCard = null;
-    editMenuTemplate.style.display = "none";
-    editMenuTemplate.getElementById("edit-card-menu-content").value = ""
 
 }
-
-// transform the input to a p and then create a new li with it input
-// function addTodo(button) {
-//     // console.log("ejecuto")   
-//     const card = button.parentNode.parentNode;
-//     cardChilds = card.childNodes;
-//     // TODO: arreglar esto 
-//     for (const child of cardChilds) {
-//         if (child.className === "todo-list") {
-//             const todoList = child;
-//             console.log("ol", todoList);
-
-//             const todoListItems = todoList.childNodes;
-//             console.log("todoListItems", todoListItems);
-//             let todoListItem = todoListItems[todoListItems.length - 2];
-//             /*
-//             for (const li of todoListItems) {
-//                 if (li.id === "todo-input") {
-//                     todoListItem = li;
-//                     break;
-//                 }
-//             }
-//             */
-//             console.log("li", todoListItem);
-            
-//             const listItemInput = todoListItem.innerHTML;
-//             console.log("input", listItemInput);
-
-//             let newPToReplaceLi = document.createElement("p");
-//             newPToReplaceLi.value = listItemInput.value;
-
-//             todoList.appendChild(todoListItem);
-//             todoListItem.replaceChild(newPToReplaceLi, listItemInput)
-//         }
-//     }
-// }
-
-// agregar funcion para agregar un nuevo item y otra para editar

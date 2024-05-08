a1f9e004ad30247ff578e18c9d8cf5ad484fce29
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-05-08 20:45:10 -0300

    Adding ignoring to link

--- UT5/navegationAndContext/src/pages/NavigationExample/index.jsx
+++ UT5/navegationAndContext/src/pages/NavigationExample/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"
+import { Link } from "react-router-dom"
 export default function NavigationExample() {
     return (
         < >
-            <p>Podemos crear un link estilo anchor utilizando <span className="highlight">import {Link} from "react-router-dom"</span></p>
+            <p>Podemos crear un link estilo anchor utilizando <span className="highlight"> import {"{Link}"} from "react-router-dom"</span></p>
 
 
         </>

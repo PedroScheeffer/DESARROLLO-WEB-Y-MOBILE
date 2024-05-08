713039a28d0ee67c1459cb5c8b53e90c6d163fea
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-05-08 20:43:13 -0300

    fix import

--- UT5/navegationAndContext/src/pages/Home/index.jsx
+++ UT5/navegationAndContext/src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 // import "./index.css";
+import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <>
             <h2>Bienvenido al ejemplo de navegacion y contexto</h2>
             <Link to={"../NavigationExample"}>Ejemplos navegacion</Link>
-
+            
         </>
     );
 }
--- UT5/navegationAndContext/src/pages/NavigationExample/index.jsx
+++ UT5/navegationAndContext/src/pages/NavigationExample/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom"
 export default function NavigationExample() {
     return (
         < >

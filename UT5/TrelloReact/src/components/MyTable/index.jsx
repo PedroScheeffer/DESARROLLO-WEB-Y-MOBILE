fdd1ab3cfd0c969cd91a423020885e0184ee818e
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-05-15 13:31:21 -0300

    typo

--- UT5/TrelloReact/src/components/MyTable/index.jsx
+++ UT5/TrelloReact/src/components/MyTable/index.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import MyColumn from '../MyColumn';
 const MyTable = ({ columns }) => {
     let keys = [];
-    console.log("Tareas resibidas " ,columns)
+    console.log("Tareas resibidas ", columns)
     return (
         <div className="table">
-            {columns?.map((task) => <MyColumn tasks={task} key={GetKey(keys)} / >)};
+            {columns?.map((task) => <MyColumn tasks={task} key={GetKey(keys)} />)}
         </div>
     );
 };
 
-function GetKey(keys){
+function GetKey(keys) {
     let uuid = self.crypto.randomUUID();
-    if(keys.includes(uuid)){
+    if (keys.includes(uuid)) {
         return GetKey(keys);
-    } 
+    }
     keys.push(uuid);
     return uuid;
 }
